98569397925c89e7964be2dea66e17096545afbf
An Thanh Le <an.thanhle@infodation.vn>
2025-06-17 14:24:23 +0700

    Add ESLint plugins for improved linting and code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,30 @@
 import tseslint from 'typescript-eslint';
 import js from '@eslint/js';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactHooks from 'eslint-plugin-react-hooks';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintImport from 'eslint-plugin-import';
 
 export default tseslint.config(
     { ignores: ['dist'] },
     {
-        extends: [
-            js.configs.recommended, ...tseslint.configs.recommended
-        ],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{js,ts,tsx}'],
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
         },
         plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'import': eslintImport,
             'simple-import-sort': simpleImportSort,
         },
         rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'import/first': 'warn',
+            'import/no-duplicates': 'warn',
             'simple-import-sort/imports': 'warn',
             'simple-import-sort/exports': 'warn',
             'no-unused-vars': 'off',
@@ -24,6 +32,5 @@ export default tseslint.config(
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/interface-name-prefix': 'off',
         }
-    }
-
+    },
 );
