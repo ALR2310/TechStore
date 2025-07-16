9374c9d4a2333a5553a27a56f198327f06b27833
An Thanh Le <an.thanhle@infodation.vn>
2025-07-16 15:03:32 +0700

    Add new 'Loa' category to header and crawler; update product links

--- scripts/crawler.ts
+++ scripts/crawler.ts
@@ -242,6 +242,7 @@ async function handleInsertProduct(productInfo: Product) {
 async function handleCrawler() {
   let page = 1;
   const collections = [
+    'loa',
     // 'tai-nghe-may-tinh',
     // 'chuot-may-tinh',
     // 'ban-phim-may-tinh',
